f84a684799fa53eda7ab1bb3ac455f40228cdda8
Jiaxi Ye <jiaxi@ucsb.edu>
2020-04-20 22:59:45 -0700

    fix scroll-to-top

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-function gototop(){
 
+window.onload = () => {
+
+    
+    this.document.getElementById('gototop').addEventListener("click", e => {
+        console.log(e)
+        window.scrollTo({ top: 0, behavior: "smooth" })
+    })
+
+    window.addEventListener('scroll', function(e) {
+        // console.log(window.scrollY)
+        var scrollrange = this.document.body.scrollHeight - this.window.innerHeight
+        if(window.scrollY < scrollrange / 2) {
+            console.log('hide')
+            // this.document.getElementById("gototop").style.visibility = 'hidden'
+            $('#gototop').hide()
+        } else {
+            console.log('show')
+            // this.document.getElementById("gototop").style.visibility = 'visible'
+            $('#gototop').show()
+        }
+    })
 }
-
-window.addEventListener('scroll', function(e) {
-    // console.log(window.scrollY)
-    var scrollrange = this.document.body.scrollHeight - this.window.innerHeight
-    if(window.scrollY < scrollrange / 2) {
-        console.log('hide')
-        // this.document.getElementById("gototop").style.visibility = 'hidden'
-        $('#gototop').hide()
-    } else {
-        console.log('show')
-        // this.document.getElementById("gototop").style.visibility = 'visible'
-        $('#gototop').show()
-    }
-})
