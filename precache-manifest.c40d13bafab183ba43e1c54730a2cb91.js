da775161aee4e7b388f833e451c641bf235905ce
Jiaxi Ye <jiaxi@ucsb.edu>
2020-04-28 13:37:06 -0700

    Updates

---
+++ precache-manifest.c40d13bafab183ba43e1c54730a2cb91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4d57d95905ae8d3b030e2fcab8a81d3",
+    "url": "/personal-website-cs185/index.html"
+  },
+  {
+    "revision": "1df29b8704d030bf3fd6",
+    "url": "/personal-website-cs185/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "5900dc983b354eb41fa0",
+    "url": "/personal-website-cs185/static/css/main.5eb21bb9.chunk.css"
+  },
+  {
+    "revision": "1df29b8704d030bf3fd6",
+    "url": "/personal-website-cs185/static/js/2.caf4160f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-website-cs185/static/js/2.caf4160f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5900dc983b354eb41fa0",
+    "url": "/personal-website-cs185/static/js/main.351d9f84.chunk.js"
+  },
+  {
+    "revision": "03f74fbaee501b6ba2d6",
+    "url": "/personal-website-cs185/static/js/runtime-main.ab7ed994.js"
+  }
+]);
