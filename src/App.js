60c10c809b1a8c99b11f38554f416345f394ac68
Jiaxi Ye <jiaxi@ucsb.edu>
2020-05-30 00:40:12 -0700

    exit debug

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            'page': 'Movies', // change me for easier debug
+            'page': 'Home', // change me for easier debug
             'overlay': 'hidden',
             'gototop': 'hidden',
             'overlayImg': require('./imgs/owls/img.gif'),
